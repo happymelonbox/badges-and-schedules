1ba72466292537a9c7bc11dffb098db9d59f6ad0
Justin Berger Howes <howes.j.j@gmail.com>
2021-08-19 08:11:29 +1000

    complete

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    newArray = []
+    array.each do |name|
+        newArray << badge_maker(name)
+    end
+    newArray
+end
+
+def assign_rooms(array)
+    roomList = []
+    array.each.with_index(1) do |value, index|
+        roomList << "Hello, #{value}! You'll be assigned to room #{index}!"
+    end
+    roomList
+end
+
+def printer(array)
+    badges = batch_badge_creator(array)
+    badges.each do |badge|
+        puts badge
+    end
+    rooms = assign_rooms(array)
+    rooms.each do |assignment|
+        puts assignment
+    end
+end
+
+
+
